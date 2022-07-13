6b5f601636dcecb1c7874bffa83c7f936bdfa08a
Morgaan Holmes <morgaan.holmes@my.scitexas.edu>
2022-07-13 11:34:36 -0400

    Introduction To CSharp
    Created an new console app and learned the basics of C# programming.

---
+++ newApp/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace newApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+
+            string greeting;
+            greeting = "Hi World!";
+            
+            Console.WriteLine(greeting);
+            
+            greeting = "Hello User!";
+            
+            Console.WriteLine(greeting);
+            
+            int age;
+            age = 38;
+
+            Console.WriteLine(age);
+
+            int sum;
+            sum = 2 + 2;
+
+            int newSum;
+            newSum = sum + 10;
+
+            Console.WriteLine(sum);
+            Console.WriteLine(newSum);
+
+            bool isOpen = false;
+
+            if (isOpen == true)
+            {
+                Console.WriteLine("Yay!");
+            }
+            else
+            {
+                Console.WriteLine("Sad day");
+            }
+            
+        }
+    }
+}
