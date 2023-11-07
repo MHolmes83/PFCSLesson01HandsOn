4508cc474b867d29f6b4323181447b092df06521
Morgaan Holmes <morgaan.holmes@my.scitexas.edu>
2023-11-07 05:37:11 -0500

    Intro to CSharp
    Started a simple dotnet project and practiced some simple started codes.

---
+++ IntroToCSharp/newApp/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Data;
+using System.Security.Cryptography.X509Certificates;
+
+namespace newApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string greeting;
+            greeting = "Hello, World!";
+
+            Console.WriteLine(greeting);
+
+            greeting = "Hi World!";
+
+            Console.WriteLine(greeting);
+
+            /*  greeting = 5;
+
+             Console.WriteLine(greeting); */
+
+            int age;
+            age = 40;
+
+            Console.WriteLine(age);
+
+            int sum;
+            sum = 2 + 2;
+            int newSum;
+            newSum = sum + 10;
+
+            Console.WriteLine(sum);
+            Console.WriteLine(newSum);
+
+
+            bool isOpen = true;
+            Console.WriteLine(isOpen);
+
+            bool isOpenAgain = false;
+
+            if (isOpenAgain == true)
+            {
+                Console.WriteLine("Yay!");
+            }
+            else
+            {
+                Console.WriteLine("Sad day");
+            }
+
+            int newAge = 25;
+            Console.WriteLine(newAge);
+
+            var name = "Morgaan Holmes";
+            Console.WriteLine(name);
+
+            double number = 2.71;
+            Console.WriteLine(number);
+
+
+
+        }
+    }
+}
+
+
