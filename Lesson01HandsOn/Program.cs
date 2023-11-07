701da7062a13aa7979e67771892acae8c1366f1d
Morgaan Holmes <morgaan.holmes@my.scitexas.edu>
2023-11-07 07:57:56 -0500

    Lesson01HandsOn
    Created a simple dotnet project that set up three booleans, each with two string variables attached and then ran a series of if...else statements that ran through the boolean and printed the appropriate variable responses to the console.

---
+++ Lesson01HandsOn/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Lesson01HandsOn
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool isSunny = false;
+            var wear = "Wear sunglasses!";
+            var dontWear = "You don't need to wear sunglasses.";
+            bool atBeach = true;
+            var sunblock = "Sunblock is a good idea.";
+            var noSunblock = "Sunblock isn't needed.";
+            bool goAnyway = true;
+            var going = "Awesome! Glad you don't mind clouds!";
+            var nextTime = "No worries! Hopefully next time we will have more sun!";
+
+            if(isSunny)
+            {
+                Console.WriteLine(wear);
+                if(atBeach)
+                {
+                    Console.WriteLine(sunblock);
+                }
+                else
+                {
+                    Console.WriteLine(noSunblock);
+                }
+            }
+            else
+            {
+                Console.WriteLine(dontWear);
+            }
+            if(goAnyway)
+            {
+                Console.WriteLine(going);
+            }
+            else
+            {
+                Console.WriteLine(nextTime);
+            }
+        }
+    }
+}
